a9a1a1472402c055c201579910d39dbbc6d39467
Deymeson <103832484+Deymeson@users.noreply.github.com>
2023-04-21 20:30:21 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    app: {
+        token: 'MTA2NzI2NDA1MjEyMTg0MTc0NQ.GeHy9e.sS3EgRns_O3AQO5CVwUVhgRHYK_UP3OW2Cqrm0',
+        playing: '< [MCPE] - POKECITY >',
+        global: true,
+        guild: 'XXX'
+    },
+
+    opt: {
+        DJ: {
+            enabled: false,
+            roleName: '',
+            commands: []
+        },
+        maxVol: 100,
+        leaveOnEnd: true,
+        loopMessage: false,
+        spotifyBridge: true,
+        defaultvolume: 75,
+        discordPlayer: {
+            ytdlOptions: {
+                quality: 'highestaudio',
+                highWaterMark: 1 << 25
+            }
+        }
+    }
+};
